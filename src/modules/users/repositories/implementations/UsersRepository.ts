3d46a75c73889314236c2f3d885703e60f0e93b8
sandersonricl <sandersonricl@gmail.com>
2022-11-20 09:51:00 -0300

    add: set files into repo

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidV4 } from "uuid";
+
 import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
@@ -19,23 +21,45 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    Object.assign(user, {
+      id: uuidV4(),
+      name,
+      email,
+      admin: false,
+      created_at: new Date(),
+    });
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const userEmail = this.users.find((user) => user.email === email);
+
+    return userEmail;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === receivedUser.id);
+
+    Object.assign(user, {
+      admin: true,
+    });
+
+    return {
+      admin: true,
+      ...user,
+    };
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,17 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlreadyInUse = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyInUse) throw new Error("Email already exists");
+
+    const userAlreadyExists = this.usersRepository.findById(name);
+
+    if (userAlreadyExists) throw new Error("User already exists");
+
+    const newUser = this.usersRepository.create({ email, name });
+
+    return newUser;
   }
 }
 
