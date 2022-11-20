234c968ae96e5c68297b9aab2fadece5753fbfe2
sandersonricl <sandersonricl@gmail.com>
2022-11-20 13:30:05 -0300

    fix: fix error test issues

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,11 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params;
+    try {
+      const { user_id } = request.params;
 
-    const userAdmin = this.turnUserAdminUseCase.execute({ user_id });
+      const userAdmin = this.turnUserAdminUseCase.execute({ user_id });
 
-    return response.json(userAdmin);
+      return response.status(200).json(userAdmin);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
